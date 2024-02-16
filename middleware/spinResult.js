3a2dc6714db2003de35ae9f292bf30a98699165c
Hundaol Negusie <hundaolnk@gmail.com>
2024-02-16 18:55:43 +0300

    ss

--- middleware/spinResult.js
+++ middleware/spinResult.js
@@ -54,7 +54,7 @@ function drawNumber(weights) {
 }
 
 function calculateWeights(players) {
-  const scalingFactor = 0.05;
+  const scalingFactor = 0.2;
   const allNumbers = Array.from({ length: 37 }, (_, i) => i);
 
   if (!players.length) {
